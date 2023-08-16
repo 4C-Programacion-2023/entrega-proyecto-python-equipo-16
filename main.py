30e729b8d2d0fb90206717b0a58e260ebd5b2fc4
MarianoNicolasFerreyra <140646312+MarianoNicolasFerreyra@users.noreply.github.com>
2023-08-16 15:56:19 -0300

    buscaminas.py

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import random
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def jugar_piedra_papel_tijera():
+    while True:
+        a = int(input("Elija 1 de estos 3 elementos: 1 = Piedra, 2 = Papel, 3 = Tijera: "))
 
+        if a < 1 or a > 3:
+            print("Valor no existente, elija uno que si exista.")
+            continue
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        b = random.randint(1, 3)
+        print(b)
 
+        if a == 1 and b == 3:
+            print("¡Ganaste! Piedra le gana a tijera")
+        elif a == 1 and b == 2:
+            print("¡Perdiste! Piedra no le gana a papel")
+        elif a == 1 and b == 1:
+            print("¡Empate!")
+        elif a == 2 and b == 1:
+            print("¡Ganaste! Papel le gana a piedra")
+        elif a == 2 and b == 2:
+            print("¡Empate!")
+        elif a == 2 and b == 3:
+            print("¡Perdiste! Papel no le gana a tijera")
+        elif a == 3 and b == 1:
+            print("¡Perdiste! Tijera no le gana a piedra")
+        elif a == 3 and b == 2:
+            print("¡Ganaste! Tijera le gana a papel")
+        elif a == 3 and b == 3:
+            print("¡Empate!")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        while True:
+            opcion = input("¿quiere jugar de nuevo? Ingresa 1 para volver a jugar, o 2 para salir: ")
+            if opcion not in ['1', '2']:
+                print("valor no existente. Elija uno que si exista.")
+            else:
+                break
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        if opcion == '2':
+            break
+
+jugar_piedra_papel_tijera()
