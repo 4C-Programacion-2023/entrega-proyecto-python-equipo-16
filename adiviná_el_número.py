60f5f3fde703eed578b11b0d19c413f7b549b78d
MarianoNicolasFerreyra <140646312+MarianoNicolasFerreyra@users.noreply.github.com>
2023-08-23 15:29:00 -0300

    Update adiviná_el_número.py

--- adiviná_el_número.py
+++ adiviná_el_número.py
@@ -2,22 +2,22 @@ import random
 
 def adiviná_el_número():
     while True:
-        numero_secreto = random.randint(1, 100)
-        intentos = 0
+        numero_secreto = random.randint(1, 100)  #el número secreto estará entre 1-100
+        intentos = 0   #los intentos se setean en 0
 
-        print("¡Bienvenido al juego de adivinanzas!")
+        print("¡Bienvenido al juego de adivinanzas!")   #bienvenida
         print("Estoy pensando en un número del 1 al 100. Adivina, tenes 10 intentos")
 
         while intentos < 10:
             try:
                 intento = int(input("Tira fruta: "))
-            except ValueError:
+            except ValueError:   #si no escribiste un número admitible, se pide que se vuelva a ingresar un número, sin gasto de turno.
                 print("Escribi bien bobi.")
                 continue
 
             intentos += 1
 
-            if intento < numero_secreto:
+            if intento < numero_secreto:   #pistas para saber cuál es tu número
                 print("El número secreto es mayor.")
             elif intento > numero_secreto:
                 print("El número secreto es menor.")
@@ -25,11 +25,11 @@ def adiviná_el_número():
                 print(f"Muy bien. Adivinaste el numero {numero_secreto} en {intento} intento")
                 break
         else:
-            print(f"Agotaste tus 10 intentos. El número secreto era {numero_secreto}. ¡ALTO BOT!")
+            print(f"Agotaste tus 10 intentos. El número secreto era {numero_secreto}. ¡ALTO BOT!")   #se revela el número secreto
 
         jugar_nuevamente = input("¿Quieres jugar de nuevo? (s/n): ")
         if jugar_nuevamente.lower() != 's':
             print("nos vemos bro")
-            break
+            break   #se pregunta si se quiere volver a jugar, o se quiere salir
 
 import random
