297411d412a433b29ea2cc874f0dbb78a43b1eae
MarianoNicolasFerreyra <140646312+MarianoNicolasFerreyra@users.noreply.github.com>
2023-08-23 15:48:45 -0300

    Update tres_en_linea.py

--- tres_en_linea.py
+++ tres_en_linea.py
@@ -1,10 +1,10 @@
-def mostrar_tablero(tablero):
+def mostrar_tablero(tablero):   #tablero
     for fila in tablero:
         print(" | ".join(fila))
         print("---------")
 
 
-def hay_empate(tablero):
+def hay_empate(tablero):   #si hay empate
     for fila in tablero:
         for casilla in fila:
             if casilla == " ":
@@ -21,14 +21,14 @@ def jugar_tres_en_linea():
         while not juego_terminado:
             mostrar_tablero(tablero)
             try:
-                fila = int(input("Ingrese el número de fila (0-2): "))
+                fila = int(input("Ingrese el número de fila (0-2): "))   #selección de donde poner la "ficha"
                 columna = int(input("Ingrese el número de columna (0-2): "))
 
                 if fila not in [0, 1, 2] or columna not in [0, 1, 2]:
                     print("Valor inválido. Ingrese un número entre 0 y 2.")
                     continue
 
-                if tablero[fila][columna] == " ":
+                if tablero[fila][columna] == " ":   #posibilidades de ubicación
                     tablero[fila][columna] = jugador_actual
                     if (tablero[0][0] == tablero[1][1] == tablero[2][2] == jugador_actual or
                             tablero[0][2] == tablero[1][1] == tablero[2][0] == jugador_actual or
@@ -49,7 +49,7 @@ def jugar_tres_en_linea():
                 print("Valor inválido. Ingrese un número entre 0 y 2.")
 
         opcion = input(
-            "¿Quiere jugar de vuelta? Ingrese 1 para volver a jugar, si no lo desea, ingrese otro parámetro: ")
+            "¿Quiere jugar de vuelta? Ingrese 1 para volver a jugar, si no lo desea, ingrese otro parámetro: ")   #volver a jugar
         if opcion != "1":
             print("Gracias por jugar. ¡Chau!")
             break
